84989aa436414f467959f156aa1a43930f88317b
Jyothish T M <jothishtm2002@gmail.com>
2025-07-04 14:34:57 +0530

    cors issue fixed

--- src/server.ts
+++ src/server.ts
@@ -50,7 +50,7 @@ app.use(helmet());
 // }));
 
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN,
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
