be991fd3fbcad358021384f90ed3bef7fd46a123
Jyothish T M <jothishtm2002@gmail.com>
2025-07-05 11:29:24 +0530

    create event form error fixed

--- src/middleware/creator/PostUpload.ts
+++ src/middleware/creator/PostUpload.ts
@@ -1,18 +1,40 @@
+// import multer from "multer";
+// import { CloudinaryStorage } from "multer-storage-cloudinary";
+// import cloudinary from "../../config/cloudinary";
+
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: async (req: any, file: any) => {
+//     return {
+//       folder: "festivia/event_gallery",
+//       allowed_formats: ["jpg", "jpeg", "png"],
+//       storage,
+//       limits: { fileSize: 10 * 1024 * 1024 }, 
+//     };
+// },
+// });
+
+
+// export const PostUpload = multer({ storage });
+
+
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../../config/cloudinary";
 
+// Setup Cloudinary storage
 const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req: any, file: any) => {
     return {
       folder: "festivia/event_gallery",
       allowed_formats: ["jpg", "jpeg", "png"],
-      storage,
-      limits: { fileSize: 10 * 1024 * 1024 }, 
     };
-},
+  },
 });
 
-
-export const PostUpload = multer({ storage });
+// Set file size limit to 10MB here
+export const PostUpload = multer({
+  storage,
+  limits: { fileSize: 10 * 1024 * 1024 } // ✅ 10 MB limit
+});
